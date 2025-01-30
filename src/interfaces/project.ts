1a8ddff0b42859b70d5db94efe1dc68b53192dbc
Kacper <kacper@home.lab>
2025-01-30 22:01:52 +0100

    build main structure

---
+++ src/interfaces/project.ts
@@ -0,0 +1,11 @@
+export default interface Project {
+    id: number;
+    slug: string;
+    title: string;
+    description: string;
+    image: any;
+    url: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,61 @@
+export const LINKS = {
+    github: 'https://github.com/thatguyKacper',
+    linkedin: 'https://www.linkedin.com/in/kacper-wojtowicz',
+    mail: 'kacper.m.wojtowicz@gmail.com',
+}
+
+export const TECH = [
+    {
+        name: "Javascript",
+        link: "",
+    },
+    {
+        name: "React",
+        link: "https://react.dev/",
+    },
+    {
+        name: "Astro",
+        link: "https://astro.build/",
+    },
+    {
+        name: "Nodejs",
+        link: "https://nodejs.org/en",
+    },
+    {
+        name: "Mongo",
+        link: "https://www.mongodb.com/",
+    },
+    {
+        name: "SQL",
+        link: "",
+    },
+]
+
+export const EXPERIENCE = [
+    {
+        company: 'Kindred.',
+        location: 'Remote',
+        position: 'Frontend Developer',
+        start: '2023',
+        link: 'https://www.kindred.cz/en/',
+        end: 'Current',
+        tasks: [
+            'Develop and maintain user interfaces using React.js, TypeScript, AstroJS, and SCSS for a global pharmaceutical client.',
+            'Collaborate with an international, cross-functional team to deliver high-quality, interactive solutions, ensuring effective communication in English.',
+            'Design and develop internal tools'
+        ],
+    },
+    {
+        company: 'SALESmanago',
+        location: 'Cracow, Poland',
+        position: 'Junior Frontend Developer',
+        link: 'https://www.salesmanago.com/',
+        start: '2022',
+        end: '2023',
+        tasks: [
+            'Create responsive landing pages, pop-ups, forms, sidebars, recommendation frames, and HTML emails',
+            "Add iframes to client's website",
+            'Develop new custom scripts and improved existing ones',
+        ],
+    },
+]
